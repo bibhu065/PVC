cd61ba09eaa4dfd9746537c9442f54a777b3cb27
Bibhuranjan Bihari <bibhu065@gmail.com>
2016-08-24 13:18:32 +0530

    Fragment Updated

---
+++ app/src/main/java/com/anchor/pvc/fragment/Base1Fragment.java
@@ -0,0 +1,25 @@
+package com.anchor.pvc.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anchor.pvc.R;
+
+/**
+ * Created by Bibhuranjan Bihari on 8/24/2016.
+ */
+public class Base1Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragmentbase_1,container,false);
+        return rootView;
+
+    }
+}
---
+++ app/src/main/java/com/anchor/pvc/fragment/Base7Fragment.java
@@ -0,0 +1,23 @@
+package com.anchor.pvc.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anchor.pvc.R;
+
+/**
+ * Created by Bibhuranjan Bihari on 8/24/2016.
+ */
+public class Base7Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragmentbase_7, container, false);
+        return rootView;
+    }
+}
