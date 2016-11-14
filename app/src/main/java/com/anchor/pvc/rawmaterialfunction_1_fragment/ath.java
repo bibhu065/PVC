e138145615b4a05b87bbdc92515f3cf94d041e0d
Bibhuranjan Bihari <bibhu065@gmail.com>
2016-11-14 10:20:49 +0530

    Raw Material Function Part-1 & Part-2 Updated

---
+++ app/src/main/java/com/anchor/pvc/rawmaterialfunction_1_fragment/ath.java
@@ -0,0 +1,21 @@
+package com.anchor.pvc.rawmaterialfunction_1_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+import com.anchor.pvc.R;
+
+/**
+ * Created by Bibhuranjan Bihari on 11/14/2016.
+ */
+public class ath extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.rmf2,container,false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/anchor/pvc/rawmaterialfunction_2_fragment/pwax.java
@@ -0,0 +1,22 @@
+package com.anchor.pvc.rawmaterialfunction_2_fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anchor.pvc.R;
+
+/**
+ * Created by Bibhuranjan Bihari on 11/14/2016.
+ */
+public class pwax extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.pwax,container,false);
+        return rootView;
+    }
+}
